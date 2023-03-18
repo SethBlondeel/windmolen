6c1c4d4d4e81ba6b1178be94ec98ee6da155489b
SethBlondeel <127240348+SethBlondeel@users.noreply.github.com>
2023-03-18 17:10:53 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,43 @@
+let draaien: number
+input.onButtonPressed(Button.A, function() {
+   draaien = 1 
+})
+input.onButtonPressed(Button.B, function () {
+    draaien = 2
+})
+
+if (draaien = 1) {
+
+    
+basic.showLeds(`
+. . # . .
+. . # . .
+. . # . .
+. . # . .
+. . # . .
+`)
+    basic.showLeds(`
+. . . . #
+. . . # .
+. . # . .
+. # . . .
+# . . . .
+`)
+basic.showLeds(`
+. . . . .
+. . . . .
+# # # # #
+. . . . .
+. . . . .
+`)
+    basic.showLeds(`
+# . . . .
+. # . . .
+. . # . .
+. . . # .
+. . . . #
+`)
+    }
+if (draaien = 2) {
+basic.clearScreen()
+}
